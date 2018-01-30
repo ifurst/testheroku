ffe0f947c8e84323976dce697e2f17a41d1ab079
Vinod Gnaniah <ifurst@outlook.com>
2018-01-30 15:21:32 +0530

    Third commit

--- server.js
+++ server.js
@@ -7,28 +7,39 @@ const port = process.env.PORT || 3000
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
-app.use(express.static(__dirname + '/public'))
 
 hbs.registerHelper('getYear', () => new Date().getFullYear() )
 hbs.registerHelper('screamit', (text) => text.toUpperCase() )
 
-app.get('/about', (q, r) => {
-    r.render('about.hbs', {
-        title: 'About Page',
-        desc: 'This is an awesome page!',
-        year: new Date().getFullYear()
-    })
-})
+app.use(express.static(__dirname + '/public'))
+
+
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         title: 'Home Page',
-        desc: 'Welcome to home page!',
-        year: new Date().getFullYear()
+        desc: 'Welcome to home page!'
+   
     })
 })
 
-app.get('/j', (q, r) => {
+app.get('/projects', (q, r) => {
+    r.render('projects.hbs', {
+        title: 'Projects Page',
+        desc: 'Placeholder for portfolio!'
+        
+    })
+})
+
+app.get('/about', (q, r) => {
+    r.render('about.hbs', {
+        title: 'About Page',
+        desc: 'This is an awesome page!'
+       
+    })
+})
+
+app.get('/propertyapi', (q, r) => {
     return r.send( {
         name: 'hello',
         age: 23,
